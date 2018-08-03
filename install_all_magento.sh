9c71080abb62f3c96f2e943dfd2e0b9e92fff31c
pcvonz <pzimmerman@bargreen.com>
2018-08-03 07:44:30 -0700

    Fix typo in install_all_magento script

--- install_all_magento.sh
+++ install_all_magento.sh
@@ -1,4 +1,4 @@
 CONTAINER=$1
 docker exec -it $CONTAINER install-sampledata
 docker exec -it $CONTAINER install-magento
-docker exec -it $CONTAINER install-vennia
+docker exec -it $CONTAINER install-venia
