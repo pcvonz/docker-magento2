a66c9c1313eec4fd0512261cfb6f80f5bdb088b0
pcvonz@gmail.com <pzimmerman@bargreen.com>
2018-11-12 12:10:44 -0800

    Update install venia script

--- install_all_magento.sh
+++ install_all_magento.sh
@@ -1,4 +1,3 @@
 CONTAINER=$(docker-compose ps web | awk 'FNR == 3 {print $1}')
-docker exec -it $CONTAINER install-sampledata
-docker exec -it $CONTAINER install-magento
 docker exec -it $CONTAINER install-venia
+docker exec -it $CONTAINER install-magento
